9b3070c7104cd0c9033550d73963206f13a843e0
Zakhar Pilipchuk <z.pilipchuk@gmail.com>
2013-05-16 23:02:14 +0400

    Show error messages to the user.

--- src/ru/altimin/hat/activities/StartGameActivity.java
+++ src/ru/altimin/hat/activities/StartGameActivity.java
@@ -1,12 +1,14 @@
 package ru.altimin.hat.activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import me.harius.hat.ConnectionError;
 import me.harius.hat.InvalidResponseError;
 import me.harius.hat.NetworkingManager;
@@ -57,6 +59,7 @@ public class StartGameActivity extends Activity {
         List<Word> words = Arrays.asList(new Word("Abra"), new Word("Cadabra"), new Word("Extermination"), new Word("Безысходность"));
         GameSettings settings = new GameSettings(words, users);
         Intent startGameIntent = new Intent(StartGameActivity.this, GameActivity.class);
+        // TODO: should we really get json, deserialize it, serialize again and finally deserialize?
         startGameIntent.putExtra("settings", settings);
         StartGameActivity.this.startActivity(startGameIntent);
     }
@@ -65,7 +68,6 @@ public class StartGameActivity extends Activity {
         NetworkingManager networkingManager = NetworkingManager.getDefault();
         RequestGameTask task = new RequestGameTask(networkingManager);
 
-        // TODO: make error visible to user
         try {
             int gameId = Integer.parseInt(gameIdInput.getText().toString());
             int gamePassword = Integer.parseInt(gamePasswordInput.getText().toString());
@@ -79,25 +81,35 @@ public class StartGameActivity extends Activity {
             Intent startGameIntent = new Intent(StartGameActivity.this, GameActivity.class);
             startGameIntent.putExtra("settings", settings);
             StartGameActivity.this.startActivity(startGameIntent);
-
         } catch (InterruptedException e) {
             Log.d(DEBUG_TAG, "InterruptedException was caught");
+            showMessage("Internal application error :'(");
         } catch (ExecutionException e) {
             Log.d(DEBUG_TAG, "ExecutionException was caught.");
             Log.d(DEBUG_TAG, e.getMessage());
             Log.d(DEBUG_TAG, e.getCause().getMessage());
+            showMessage("Internal application error :'(");
         } catch (ConnectionError connectionError) {
             Log.d(DEBUG_TAG, "ConnectionError was caught:\n" +
                     connectionError.getMessage());
             Log.d(DEBUG_TAG, connectionError.getCause().getMessage());
+            showMessage("Internet connection problems");
         } catch (InvalidResponseError invalidResponseError) {
             Log.d(DEBUG_TAG, "InvalidResponseError was caught:\n" +
                     invalidResponseError.getMessage());
             Log.d(DEBUG_TAG, invalidResponseError.getCause().getMessage());
+            showMessage("Invalid response from the server");
         } catch (NumberFormatException formatError) {
             Log.d(DEBUG_TAG, "One of fields contains non-integer");
+            showMessage("One of fields contains non-integer");
         }
 
 
+    }
+
+    private void showMessage(String text) {
+        Context context = this.getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context,text, duration);
     }
 }
